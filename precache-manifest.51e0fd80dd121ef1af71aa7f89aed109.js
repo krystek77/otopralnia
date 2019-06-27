8543e66c268bdd450c69b086d2ff85499642ce68
krystek77 <krystian.wrona77@gmail.com>
2019-06-27 11:36:22 +0200

    Updates

---
+++ precache-manifest.51e0fd80dd121ef1af71aa7f89aed109.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc1a22097e1e4ca859cfc28aacb8fcf0",
+    "url": "/otopralnia/static/media/coin_rx.fc1a2209.png"
+  },
+  {
+    "revision": "7dc61d95252bc59934b9",
+    "url": "/otopralnia/static/css/main.9450900e.chunk.css"
+  },
+  {
+    "revision": "a00823d9833fe8d02455",
+    "url": "/otopralnia/static/js/runtime~main.a00823d9.js"
+  },
+  {
+    "revision": "41498dcb9c946be87969",
+    "url": "/otopralnia/static/js/2.41498dcb.chunk.js"
+  },
+  {
+    "revision": "96c2a0e3d5d0b7f9ef4ccc0270af8a36",
+    "url": "/otopralnia/static/media/home.96c2a0e3.jpg"
+  },
+  {
+    "revision": "ed3f4c1ef895b6da9ace878c9591711a",
+    "url": "/otopralnia/static/media/customers.ed3f4c1e.jpg"
+  },
+  {
+    "revision": "49598cabe16efadba47b44afc09f76cc",
+    "url": "/otopralnia/static/media/contact.49598cab.jpg"
+  },
+  {
+    "revision": "ca16670ba8207cdb14ee4199e23a0e3e",
+    "url": "/otopralnia/static/media/aboutUs.ca16670b.jpg"
+  },
+  {
+    "revision": "ed3f4c1ef895b6da9ace878c9591711a",
+    "url": "/otopralnia/static/media/equipment.ed3f4c1e.jpg"
+  },
+  {
+    "revision": "ed3f4c1ef895b6da9ace878c9591711a",
+    "url": "/otopralnia/static/media/blog.ed3f4c1e.jpg"
+  },
+  {
+    "revision": "ed3f4c1ef895b6da9ace878c9591711a",
+    "url": "/otopralnia/static/media/recycling.ed3f4c1e.jpg"
+  },
+  {
+    "revision": "78b647470987cbd365fba943fbbaee13",
+    "url": "/otopralnia/static/media/technology.78b64747.jpg"
+  },
+  {
+    "revision": "054a39d5ec32c90601c3a1f7fa2c1262",
+    "url": "/otopralnia/static/media/bh_bezwirowania.054a39d5.png"
+  },
+  {
+    "revision": "01280f9e8e9d8cfde8b5623e502ee899",
+    "url": "/otopralnia/static/media/bh_normalnoobrotowe.01280f9e.png"
+  },
+  {
+    "revision": "34e77e5c2ad17bf66887285aff8350c9",
+    "url": "/otopralnia/static/media/bh_wysokoobrotowe.34e77e5c.png"
+  },
+  {
+    "revision": "f17e1311e21873916bb2ef88d0fa3de6",
+    "url": "/otopralnia/static/media/coin_dx.f17e1311.png"
+  },
+  {
+    "revision": "149034026bab737c640de06a92d44d8d",
+    "url": "/otopralnia/static/media/coin_pralnicowirowki_wysokoobrotowe.14903402.png"
+  },
+  {
+    "revision": "7dc61d95252bc59934b9",
+    "url": "/otopralnia/static/js/main.7dc61d95.chunk.js"
+  },
+  {
+    "revision": "99160b6842f6b23a468ca0ec16ae1abb",
+    "url": "/otopralnia/static/media/coin_sdc10.99160b68.png"
+  },
+  {
+    "revision": "32d20e1eb2ca5cac1f8ec39f5b065309",
+    "url": "/otopralnia/static/media/coin_sdsc10.32d20e1e.png"
+  },
+  {
+    "revision": "64a92b05f1f6d4572acf24fe47cc491d",
+    "url": "/otopralnia/static/media/coin_spc10.64a92b05.png"
+  },
+  {
+    "revision": "d733c83e3110047ed42de837ef05c22d",
+    "url": "/otopralnia/static/media/coin_spsc10.d733c83e.png"
+  },
+  {
+    "revision": "ad588dae3445338ff62105c52fa9baa3",
+    "url": "/otopralnia/static/media/pralnice_boczne.ad588dae.png"
+  },
+  {
+    "revision": "1fd2e6cc7d91297283a2eff0f0de817f",
+    "url": "/otopralnia/static/media/pralnice_czolowe.1fd2e6cc.png"
+  },
+  {
+    "revision": "178c8ecad7326ea2a922be8155ad5789",
+    "url": "/otopralnia/static/media/pralnicowirowki_normalnoobrotowe.178c8eca.png"
+  },
+  {
+    "revision": "93a242c2577a628fa3c7d8d69c5cd438",
+    "url": "/otopralnia/static/media/pralnicowirowki_szybkoobrotowe.93a242c2.png"
+  },
+  {
+    "revision": "0b2999dbb3de32224738e6c2b8a05221",
+    "url": "/otopralnia/static/media/pralnicowirowki_wysokoobrotowe.0b2999db.png"
+  },
+  {
+    "revision": "3e0bf377d454b84a07eb6a5e71f72dd9",
+    "url": "/otopralnia/static/media/prasownice_nieckowe.3e0bf377.png"
+  },
+  {
+    "revision": "cd36ea658f2979e168ae9a0eb72cc4e5",
+    "url": "/otopralnia/static/media/prasownice_walcowe.cd36ea65.png"
+  },
+  {
+    "revision": "cffd9161e83eef6a89e607d13f097611",
+    "url": "/otopralnia/static/media/suszarki_DX.cffd9161.png"
+  },
+  {
+    "revision": "887f65e3a367a6667de308b304af32dd",
+    "url": "/otopralnia/static/media/suszarki_S.887f65e3.png"
+  },
+  {
+    "revision": "66325b9032c0bbd53dc2bfac435d35b6",
+    "url": "/otopralnia/static/media/suszarki_T.66325b90.png"
+  },
+  {
+    "revision": "d68e9600404a4c1aeb8c4cb301827a5a",
+    "url": "/otopralnia/static/media/wirowki.d68e9600.png"
+  },
+  {
+    "revision": "1aea2b6a62d6151fd6ef3a43930b1557",
+    "url": "/otopralnia/static/media/FX_COIN.1aea2b6a.jpeg"
+  },
+  {
+    "revision": "95a9b0b85c67b07e957f8792cffc994c",
+    "url": "/otopralnia/index.html"
+  }
+];
