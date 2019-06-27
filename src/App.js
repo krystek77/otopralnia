0a20331cf9b0578c0067fb8a47cfc8ae5694727d
krystek77 <krystian.wrona77@gmail.com>
2019-06-27 12:16:56 +0200

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 
-
-
 class App extends Component {
-
- 
-  render() {
-    return (
-          <Router>
-            <Layout/>
-          </Router>
-    )
-  }
+	render() {
+		return (
+			<Router basename={process.env.PUBLIC_URL}>
+				<Layout />
+			</Router>
+		);
+	}
 }
 
 export default App;
